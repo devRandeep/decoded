8554ee45a1a8f202ad2a4b2bd521e6e9a6636ea9
reactgame <raghavrana997@gmail.com>
2023-12-22 18:54:46 +0530

    today update

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,10 @@ import whoWeAreImage from "../images/whoweare.png";
 export default function About() {
   const OurPartnersLogo = [
     {
-      imageURL: "./images/ourpartners1.png",
+      imageURL: "../assets/images/ourpartners1.png",
     },
   ];
+
   return (
     <>
       {/* Breadcrumb */}
@@ -18,21 +19,14 @@ export default function About() {
       <section className="aboutSection p80">
         <Container>
           <Row className="align-items-center">
-            <Col md={6} className="whoweAreImage">
+            <Col md={6} className="whoweAreImage">  
               <img src={whoWeAreImage} alt="" />
             </Col>
             <Col md={6}>
               <div className="aboutContent">
                 <h3>Who We Are</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  {/* Your content */}
                 </p>
               </div>
             </Col>
@@ -46,8 +40,8 @@ export default function About() {
             <div className="ourPartnersContent">
               <h3 className="text-center">Our Partners</h3>
             </div>
-            {OurPartnersLogo.map((partnerLogo) => (
-              <Col md={3}>
+            {OurPartnersLogo.map((partnerLogo, index) => (
+              <Col md={3} key={index}>
                 <img src={partnerLogo.imageURL} alt="" />
               </Col>
             ))}
